491f21485c62bad5c0903da1898832b91f3619ca
toneiojimz <ajimeneza77@gmail.com>
2019-12-05 15:34:39 -0800

    app created

---
+++ app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Let's code like a programmer.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn to React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
